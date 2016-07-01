79f294e905f95bafe6153c8a0b35047526522deb
K.Chan <chandankumar99341@gmail.com>
2016-07-01 15:52:49 +0530

    app complete

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 ### User Story
 
-1. User Story: I can submit a FormData object that includes a file upload.
-2. User Story: When I submit something, I will receive the file size in bytes within the JSON response
+1. I can submit a FormData object that includes a file upload.
+2. When I submit something, I will receive the file size in bytes within the JSON response
+
+
+
+Helpful Links:
+https://www.npmjs.com/package/formidable
+http://stackoverflow.com/questions/7042340/node-js-error-cant-set-headers-after-they-are-sent
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
+var formidable = require('formidable');
+var app = express();
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'File Upload' });
+});
+
+router.post('/upload', function (req, res) {
+	// parse incoming payload (file/multipart)
+	var form = new formidable.IncomingForm();
+
+	form.parse(req, function(err, fields, files) {
+		var fileSize = 0;
+		if(files.anyfile.size) {
+			fileSize = files.anyfile.size;
+			res.send({
+				fileSize: fileSize + ' bytes'
+			});
+			res.end();
+		} else {
+			res.send({
+				fileSize: "Can't count, file is too big!"
+			});
+			res.end();
+		}
+  });
+
 });
 
 module.exports = router;
